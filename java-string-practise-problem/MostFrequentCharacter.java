ac4ead9962cf54660f02a7fd63c963043f62bf40
akki2308 <95994989+akki2308@users.noreply.github.com>
2025-03-07 13:49:25 +0530

    Update MostFrequentCharacter.java

--- java-string-practise-problem/MostFrequentCharacter.java
+++ java-string-practise-problem/MostFrequentCharacter.java
@@ -2,31 +2,26 @@ import java.util.Scanner;
 
 public class MostFrequentCharacter {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // Input the string
         System.out.print("Enter a string: ");
-        String str = scanner.nextLine();
+        String str = sc.nextLine();
 
-        int[] freq = new int[256]; // Array to store frequency of characters (ASCII range)
-
-        // Count frequency of each character
-        for (int i = 0; i < str.length(); i++) {
-            freq[str.charAt(i)]++;
-        }
-
-        // Find the character with the highest frequency
-        char mostFrequent = str.charAt(0);
+        int[] freq = new int[256]; // Frequency array
+        char mostFrequent = ' ';
         int maxFreq = 0;
-        
+
         for (int i = 0; i < str.length(); i++) {
-            if (freq[str.charAt(i)] > maxFreq) {
-                maxFreq = freq[str.charAt(i)];
-                mostFrequent = str.charAt(i);
+            char ch = str.charAt(i);
+            freq[ch]++;
+
+            if (freq[ch] > maxFreq) {
+                maxFreq = freq[ch];
+                mostFrequent = ch;
             }
         }
 
         System.out.println("Most frequent character: '" + mostFrequent + "' (appears " + maxFreq + " times)");
-        scanner.close();
+        sc.close();
     }
 }
