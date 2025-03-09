1e5132659eec2cabaf8ca77d346415bd8f54f46f
akki2308 <95994989+akki2308@users.noreply.github.com>
2025-03-09 23:41:18 +0530

    Update MostFrequentCharacter.java

--- java-string-practise-problem/MostFrequentCharacter.java
+++ java-string-practise-problem/MostFrequentCharacter.java
@@ -1,27 +1,44 @@
 import java.util.Scanner;
 
 public class MostFrequentCharacter {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
-        String str = sc.nextLine();
+    public static char mostFrequentChar(String str) {
+        int maxCount = 0;
+        char mostFrequentChar = '\0';
 
-        int[] freq = new int[256]; // Frequency array
-        char mostFrequent = ' ';
-        int maxFreq = 0;
-
+        // Loop through each character in the string
         for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            freq[ch]++;
+            char currentChar = str.charAt(i);
+            int count = 0;
 
-            if (freq[ch] > maxFreq) {
-                maxFreq = freq[ch];
-                mostFrequent = ch;
+            // Count the occurrences of currentChar in the string
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == currentChar) {
+                    count++;
+                }
+            }
+
+            // Update the most frequent character if necessary
+            if (count > maxCount) {
+                maxCount = count;
+                mostFrequentChar = currentChar;
             }
         }
 
-        System.out.println("Most frequent character: '" + mostFrequent + "' (appears " + maxFreq + " times)");
+        return mostFrequentChar;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Take input from the user
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        
+        // Find and print the most frequent character
+        char result = mostFrequentChar(str);
+        System.out.println("Most Frequent Character: '" + result + "'");
+        
         sc.close();
     }
 }
